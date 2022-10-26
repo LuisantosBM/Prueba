07b2eb67ecc0eea123162aa741bb007fe75d6c00
Luisantos <97645732+LuisantosBM@users.noreply.github.com>
2022-10-26 09:02:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,8 @@ df = pd.DataFrame({
 
 st.write(df)
 
-st.map(df)
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
