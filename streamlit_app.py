bb7056cbfe704fbff2c14d5b080f4d595c8d21e4
Luisantos <97645732+LuisantosBM@users.noreply.github.com>
2023-08-28 22:16:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,29 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import time
 
-# Carga de datos existentes (si los hay)
+# Cargar el archivo CSV en un DataFrame de pandas
 try:
     df = pd.read_csv("data.csv")
 except FileNotFoundError:
-    df = pd.DataFrame(columns=["Nombre", "Respuesta"])
+    df = pd.DataFrame(columns=["Opcion"])
 
-# Pregunta e ingreso de nombre
-nombre = st.text_input("Ingresa tu nombre:")
-respuesta = st.selectbox("Selecciona una respuesta:", ["Opción 1", "Opción 2", "Opción 3", "Opción 4", "Opción 5"])
+# Desplegar un selector con 4 opciones
+opcion = st.selectbox("Elige una opción:", ["Opción 1", "Opción 2", "Opción 3", "Opción 4"])
 
-# Botón para enviar
+# Botón para registrar la elección del usuario
 if st.button("Enviar"):
-    # Añadir nueva fila al DataFrame
-    new_row = {"Nombre": nombre, "Respuesta": respuesta}
+    # Añadir la selección del usuario al DataFrame
+    new_row = {"Opcion": opcion}
     df = df.append(new_row, ignore_index=True)
-    
-    # Guardar el DataFrame actualizado en el archivo CSV
+    # Guardar el DataFrame en el archivo CSV
     df.to_csv("data.csv", index=False)
 
-# Reservar un espacio para la gráfica
-placeholder = st.empty()
-
-# Actualizar la gráfica en tiempo real
-while True:
-    # Leer los datos
-    df = pd.read_csv("data.csv")
-    
-    # Crear la gráfica de barras
-    fig, ax = plt.subplots()
-    df["Respuesta"].value_counts().plot(kind='bar', ax=ax)
-    ax.set_title("Respuestas en tiempo real")
-    
-    # Mostrar la gráfica en el espacio reservado
-    placeholder.pyplot(fig)
-    
-    # Pausa antes de actualizar
-    time.sleep(2)
+# Crear y mostrar un gráfico de barras basado en las selecciones
+st.subheader("Resultados")
+df["Opcion"].value_counts().plot(kind="bar")
+plt.xlabel("Opciones")
+plt.ylabel("Número de selecciones")
+plt.title("Selecciones de los usuarios")
+st.pyplot()
 
