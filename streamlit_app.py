03def82e5cf0b529e30143b23cd75579c46af2bb
Luisantos <97645732+LuisantosBM@users.noreply.github.com>
2023-08-28 22:32:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,27 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Función para crear la tabla si no existe
 def create_table():
-    conn = sqlite3.connect("data.db")
+    conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS opciones (opcion TEXT)")
+    c.execute('CREATE TABLE IF NOT EXISTS opciones (opcion TEXT)')
     conn.commit()
     conn.close()
 
 # Función para añadir una opción a la tabla
 def add_opcion(opcion):
-    conn = sqlite3.connect("data.db")
+    conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    c.execute("INSERT INTO opciones (opcion) VALUES (?)", (opcion,))
+    c.execute('INSERT INTO opciones (opcion) VALUES (?)', (opcion,))
     conn.commit()
     conn.close()
 
 # Función para leer las opciones desde la tabla
 def read_opciones():
-    conn = sqlite3.connect("data.db")
-    df = pd.read_sql_query("SELECT * FROM opciones", conn)
+    conn = sqlite3.connect('data.db')
+    df = pd.read_sql_query('SELECT * FROM opciones', conn)
     conn.close()
     return df
 
@@ -30,23 +29,20 @@ def read_opciones():
 create_table()
 
 # Permitir al usuario seleccionar una opción
-opcion_seleccionada = st.selectbox("Elige una opción:", ["Opción 1", "Opción 2", "Opción 3", "Opción 4"])
+opcion_seleccionada = st.selectbox('Elige una opción:', ['Opción 1', 'Opción 2', 'Opción 3', 'Opción 4'])
 
 # Botón para registrar la selección
-if st.button("Enviar"):
+if st.button('Enviar'):
     add_opcion(opcion_seleccionada)
-    st.write(f"Has seleccionado: {opcion_seleccionada}")
+    st.write(f'Has seleccionado: {opcion_seleccionada}')
 
 # Mostrar las selecciones anteriores en una tabla
-st.subheader("Selecciones Anteriores")
+st.subheader('Selecciones Anteriores')
 df = read_opciones()
 st.write(df)
 
 # Crear y mostrar un gráfico de barras con las selecciones
-st.subheader("Gráfico de las selecciones")
-df['opcion'].value_counts().plot(kind='bar')
-plt.xlabel('Opciones')
-plt.ylabel('Conteo')
-plt.title('Distribución de las selecciones')
-st.pyplot()
+st.subheader('Gráfico de las Selecciones')
+conteo_opciones = df['opcion'].value_counts()
+st.bar_chart(conteo_opciones)
 
