5d3bacac9b192beea19eeac90ec7a3d10396e611
Luisantos <97645732+LuisantosBM@users.noreply.github.com>
2023-08-28 22:13:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,43 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-st.write("""
-#Hola de nuevo
-Hola :V
-""")
-st.title('Hola xD')
+# Carga de datos existentes (si los hay)
+try:
+    df = pd.read_csv("data.csv")
+except FileNotFoundError:
+    df = pd.DataFrame(columns=["Nombre", "Respuesta"])
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+# Pregunta e ingreso de nombre
+nombre = st.text_input("Ingresa tu nombre:")
+respuesta = st.selectbox("Selecciona una respuesta:", ["Opción 1", "Opción 2", "Opción 3", "Opción 4", "Opción 5"])
 
-st.write(df)
+# Botón para enviar
+if st.button("Enviar"):
+    # Añadir nueva fila al DataFrame
+    new_row = {"Nombre": nombre, "Respuesta": respuesta}
+    df = df.append(new_row, ignore_index=True)
+    
+    # Guardar el DataFrame actualizado en el archivo CSV
+    df.to_csv("data.csv", index=False)
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+# Reservar un espacio para la gráfica
+placeholder = st.empty()
 
-st.write(map_data)
+# Actualizar la gráfica en tiempo real
+while True:
+    # Leer los datos
+    df = pd.read_csv("data.csv")
+    
+    # Crear la gráfica de barras
+    fig, ax = plt.subplots()
+    df["Respuesta"].value_counts().plot(kind='bar', ax=ax)
+    ax.set_title("Respuestas en tiempo real")
+    
+    # Mostrar la gráfica en el espacio reservado
+    placeholder.pyplot(fig)
+    
+    # Pausa antes de actualizar
+    time.sleep(2)
 
-st.map(map_data)
