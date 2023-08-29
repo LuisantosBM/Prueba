75ad8d9b25abb958262b37f9700209d278481add
Luisantos <97645732+LuisantosBM@users.noreply.github.com>
2023-08-28 22:18:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,8 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
-# Cargar el archivo CSV en un DataFrame de pandas
-try:
-    df = pd.read_csv("data.csv")
-except FileNotFoundError:
-    df = pd.DataFrame(columns=["Opcion"])
+# Permitir al usuario seleccionar una opción
+opcion_seleccionada = st.selectbox("Elige una opción:", ["Opción 1", "Opción 2", "Opción 3", "Opción 4"])
 
-# Desplegar un selector con 4 opciones
-opcion = st.selectbox("Elige una opción:", ["Opción 1", "Opción 2", "Opción 3", "Opción 4"])
-
-# Botón para registrar la elección del usuario
+# Botón para registrar la selección
 if st.button("Enviar"):
-    # Añadir la selección del usuario al DataFrame
-    new_row = {"Opcion": opcion}
-    df = df.append(new_row, ignore_index=True)
-    # Guardar el DataFrame en el archivo CSV
-    df.to_csv("data.csv", index=False)
-
-# Crear y mostrar un gráfico de barras basado en las selecciones
-st.subheader("Resultados")
-df["Opcion"].value_counts().plot(kind="bar")
-plt.xlabel("Opciones")
-plt.ylabel("Número de selecciones")
-plt.title("Selecciones de los usuarios")
-st.pyplot()
-
+    st.write(f"Has seleccionado: {opcion_seleccionada}")
